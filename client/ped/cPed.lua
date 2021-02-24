107d558c6391990e737a1e47bd03dd27df670b23
benank <terkolskie@gmail.com>
2021-02-23 17:31:17 -0800

    Add Ped:SetHeadOverlay

--- client/ped/cPed.lua
+++ client/ped/cPed.lua
@@ -632,6 +632,27 @@ function Ped:IsGettingUp()
     return IsPedGettingUp(self.ped_id)
 end
 
+--[[
+    OverlayID ranges from 0 to 12, index from 0 to _GET_NUM_OVERLAY_VALUES(overlayID)-1, and opacity from 0.0 to 1.0.   
+    overlayID       Part                  Index, to disable  
+    0               Blemishes             0 - 23, 255  
+    1               Facial Hair           0 - 28, 255  
+    2               Eyebrows              0 - 33, 255  
+    3               Ageing                0 - 14, 255  
+    4               Makeup                0 - 74, 255  
+    5               Blush                 0 - 6, 255  
+    6               Complexion            0 - 11, 255  
+    7               Sun Damage            0 - 10, 255  
+    8               Lipstick              0 - 9, 255  
+    9               Moles/Freckles        0 - 17, 255  
+    10              Chest Hair            0 - 16, 255  
+    11              Body Blemishes        0 - 11, 255  
+    12              Add Body Blemishes    0 - 1, 255  
+]]
+function Ped:SetHeadOverlay(overlayID, index, opacity)
+    SetPedHeadOverlay(self.ped_id, overlayID, index, tofloat(opacity))
+end
+
 
 PedTypes =
 {  
